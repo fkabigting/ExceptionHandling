727b70c3ea183803d540bebab6e7f5012dfa1623
fkabigting <leonardfrancis.kabigting1@my-aolcc.com>
2024-01-07 00:58:53 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ExceptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try //this command will run the code below 
+            {
+                Console.WriteLine("Pick a number.");
+                int num1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Pick another number.");
+                int num2 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Quotien is: " + (num1 / num2));
+                Console.ReadLine();
+
+            }
+            catch(FormatException ex) //the catch will catch an error if the format inputted by the user does not meet the datatype - in this case it's an int data type
+            {
+                //Console.WriteLine(ex.Message);//this is a system generated message
+                Console.WriteLine("Please enter a whole number");
+            }
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}
